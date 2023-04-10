cd737c7d60cdb23ebaacfcd82f6b25947292e3a7
Ethan Vaughn <ethancvaughn@gmail.com>
2023-04-10 17:13:07 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace fag_intex.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
